f972d732f28a98778efc736a28239bfd6b571e13
preetkakkar <107094417+preetkakkar@users.noreply.github.com>
2024-11-01 12:13:14 +0800

    Update registration.php

--- ProductBreadcrumbs/registration.php
+++ ProductBreadcrumbs/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'AMI_ProductBreadcrumbs',
+    'MagentoP_ProductBreadcrumbs',
     __DIR__
 );
